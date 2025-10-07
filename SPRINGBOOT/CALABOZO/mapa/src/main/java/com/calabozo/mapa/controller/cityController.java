19899683848efd5faec88d500876204ba6f375cf
mabajim065 <mariaabascal06@gmail.com>
2025-10-07 21:28:46 +0200

    07/10/25

---
+++ SPRINGBOOT/CALABOZO/mapa/src/main/java/com/calabozo/mapa/controller/cityController.java
@@ -0,0 +1,54 @@
+package com.calabozo.mapa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.calabozo.mapa.model.Ciudad;
+import com.calabozo.mapa.repository.CiudadRepository;
+
+import jakarta.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/ciudades")
+public class cityController {
+
+    @Autowired
+    private CiudadRepository ciudadRepository;
+
+    @GetMapping
+    public String listCities(Model model) {
+
+        // Cargamos todas las ciudades de BD
+        List<Ciudad> listaCiudades = ciudadRepository.findAll();
+
+        // Cargamos en model los datos de la lista de ciudades para que
+        // le llegue a la pagina web destino (vista)
+        model.addAttribute("ciudades", listaCiudades);
+
+        return "listaCiudades";
+
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String removeCity(@PathVariable Long id, RedirectAttributes redAttrib) {
+
+        if (!ciudadRepository.existsById(id))
+            redAttrib.addFlashAttribute("error", "La ciudad no Existe");
+        else {
+            ciudadRepository.deleteById(id);
+            redAttrib.addFlashAttribute("sucess", "Se ha borrado Correctamente la ciudad con id " + id);
+        }
+
+        return "redirect:/ciudades";
+    }
+
+}
