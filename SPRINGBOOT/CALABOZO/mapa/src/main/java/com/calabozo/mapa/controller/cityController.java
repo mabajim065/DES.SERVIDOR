a39aad9a18840ad1df806cab64ae286bb1195cde
MARÍA ABASCAL <mariaabascal06@gmail.com>
2025-10-09 12:21:15 +0200

    pablo

--- SPRINGBOOT/CALABOZO/mapa/src/main/java/com/calabozo/mapa/controller/cityController.java
+++ SPRINGBOOT/CALABOZO/mapa/src/main/java/com/calabozo/mapa/controller/cityController.java
@@ -13,9 +13,12 @@ import com.calabozo.mapa.repository.CiudadRepository;
 import jakarta.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/ciudades")
@@ -49,6 +52,63 @@ public class cityController {
         }
 
         return "redirect:/ciudades";
+    }
+
+    @GetMapping("/nuevo")
+    public String newCity(Model model) {
+
+        // Creamos una ciudad para que el formulario
+        // la asocie a sus datos
+        Ciudad ciudad = new Ciudad();
+        // La guardamos en el model para que le llegue al formulario
+        model.addAttribute("ciudad", ciudad);
+        // Cargamos la vista nuevaCiudad
+        return "nuevaCiudad";
+    }
+
+    @PostMapping("/crear")
+    public String createCity(@ModelAttribute("ciudad") Ciudad ciudad) {
+
+        ciudadRepository.save(ciudad);
+
+        return "redirect:/ciudades";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editCity(@PathVariable Long id, Model model) {
+
+        // Primero creo una ciudad en blanco
+        Ciudad ciudad = new Ciudad();
+
+        // Si no existe la ciudad con id en la bd devuelo el error
+        if (!ciudadRepository.existsById(id))
+            model.addAttribute("error", "La ciudad no Existe");
+        // Si existe la ciudad en bd la cargamos
+        else
+            ciudad = ciudadRepository.findById(id).get();
+        // Cargamos la ciudad en el model y cargamos la vista
+        model.addAttribute("ciudad", ciudad);
+        // TODO la fecha no la coge bien el type=date de html5 al estar guardada
+        // en formato americano en bd y intentar mostrarse en un navegador con formato
+        // español
+        // todo:solucionarlo
+        return "editarCiudad";
+    }
+
+    @PostMapping("/modificar")
+    public String modifyCity(@ModelAttribute("ciudad") Ciudad ciudad, Model model) {
+
+        try {
+            // Guardamos la ciudad
+            ciudadRepository.save(ciudad);
+
+        } catch (Exception e) {
+            model.addAttribute("error", "La ciudad no Existe");
+            return "redirect:/ciudades";
+        }
+
+        return "redirect:/ciudades";
+
     }
 
 }
